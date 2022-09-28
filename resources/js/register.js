efe65e9aaaa378169f76478142335307058b2688
Effendi Nugraha <effendi94.autobot@gmail.com>
2022-09-28 15:26:11 +0700

    create register js

---
+++ resources/js/register.js
@@ -0,0 +1,17 @@
+// const toggleLoader = (dom) => {
+//     let e = dom.nextElementSibling;
+//     e.classList.toggle("hide");
+// };
+
+// module.exports = {
+//     toggleLoader,
+// };
+
+window.showLoader = (dom) => {
+    // let e = dom.nextElementSibling;
+    // e.classList.remove("hide");
+};
+
+window.hideLoader = (dom) => {};
+
+window.checkUsername = async (dom) => {};
